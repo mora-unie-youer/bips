14eab2457087dd1eda09d95391913f79f99517fa
Mora Unie Youer <mora_unie_youer@riseup.net>
2022-02-27 21:17:04 +0000

    Making usage of new UnoCSS rules

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,14 @@ const App: Component = () => {
 
 	return (
 		<>
-			<p class="text-4xl text-green-700 text-center py-20">
+			<p text-4xl text-green-700 text-center py-20 animate-bounce-alt animate-count-infinite animate-duration-3s>
 				Hello <a
-					class="text-pink-600 hover:font-bold hover:border-1"
+					text-pink-600 hover:font-bold hover:border-1
 					href="https://antfu.me/posts/reimagine-atomic-css"
 					target="atomic-css"
 				>Atomic CSS</a>!
 			</p>
-			<main class="ma4">
+			<main m-4>
 				<Route />
 			</main>
 		</>
