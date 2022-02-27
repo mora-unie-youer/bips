ce2e7bec6a802bdc02484a70e1d4476e4fbf1afe
Mora Unie Youer <mora_unie_youer@riseup.net>
2022-02-27 21:08:07 +0000

    Importing `uno` preset to `unocss.config.ts`

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from "@unocss/vite";
 import presetAttributify from "@unocss/preset-attributify";
 import presetIcons from "@unocss/preset-icons";
-import { presetMini } from "@unocss/preset-mini";
+import { presetUno } from "@unocss/preset-uno";
 import presetTypography from "@unocss/preset-typography";
 import presetWebFonts from "@unocss/preset-web-fonts";
 
 export default defineConfig({
 	presets: [
-		presetMini(),
+		// @ts-ignore
+		presetUno(),
 		// @ts-ignore
 		presetIcons(),
 		// @ts-ignore
